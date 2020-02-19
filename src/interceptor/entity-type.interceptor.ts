88fbacb5b3267d8dc8ddf7e2d86be993f5d8be67
Clément Haller <clement.haller95@hotmail.fr>
2020-02-19 15:29:57 +0100

    reverse true false isPoi

--- src/interceptor/entity-type.interceptor.ts
+++ src/interceptor/entity-type.interceptor.ts
@@ -7,7 +7,7 @@ export class EntityTypeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
     return next.handle().pipe(map(data => {
       data = data.map((d) => {
-        d.isPoi = typeof d.domainMail !== 'undefined';
+        d.isPoi = typeof d.domainMail === 'undefined';
         return d;
       });
       return data;
