d806be4ea49b76ccbb94c60d09d96b66e685c368
Clément Haller <clement.haller95@hotmail.fr>
2020-02-20 17:48:43 +0100

    ad limit each

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.use(
     rateLimit({
       windowMs: 5 * 60 * 1000, // 15 minutes
-      max: 1000, // limit each IP to 100 requests per windowMs
+      max: 10000, // limit each IP to 100 requests per windowMs
     }),
   );
   const options = new DocumentBuilder()
